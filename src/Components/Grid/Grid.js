10a17ffa315d5fef81a3b1e343ea82e47b538c19
Akshada Tambe <akshada.tambe33@gmail.com>
2024-06-04 00:58:59 +0530

    Added eval function

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -4,7 +4,6 @@ import { useState } from 'react';
 
 function Grid() {
     let [data, setData] = useState("");
-    let [equation,setEquation]=useState("");
     const parentToChild = (a) => {
         setData(data + a);
         console.log("data "+data);
@@ -13,73 +12,15 @@ function Grid() {
       const clear = () => {
         setData("");
       }
-      const calculate=(oldEquation)=>{
-        setEquation(oldEquation);
-        for (let i = 0; i < equation.length; i++) {
-          console.log(equation[i]);
-          if(equation[i]==="/")
-            {
-              let nums=equation.toString().split(equation[i]);
-              let ans=nums[0].charAt(nums[0].length) / nums[1].charAt(0);
-              setData(equation.substring(0,nums[0].length)+ans+nums[1].substring(1,equation.length));
-            }
-          if(equation[i]==="*")
-            {
-              let nums=equation.toString().split(equation[i]);
-              let ans=nums[0].charAt(nums[0].length) * nums[1].charAt(0);
-              setData(equation.substring(0,nums[0].length)+ans+nums[1].substring(1,equation.length));
-            }
-          if(equation[i]==="+")
-            {
-              let nums=equation.toString().split(equation[i]);
-              let ans=Number(nums[0].charAt(nums[0].length)) + Number(nums[1].charAt(0));
-              console.log("ans "+ans);
-              setData(equation.substring(0,nums[0].length)+ans+nums[1].substring(1,equation.length));
-            }
-          if(equation[i]==="-")
-            {
-              let nums=equation.toString().split(equation[i]);
-              let ans=nums[0].charAt(nums[0].length) - nums[1].charAt(0);
-              setData(equation.substring(0,nums[0].length)+ans+nums[1].substring(1,equation.length));
-            }
-          if(equation[i]==="%")
-           {
-              let nums=equation.toString().split(equation[i]);
-              let ans=nums[0].charAt(nums[0].length) % nums[1].charAt(0);
-              setData(equation.substring(0,nums[0].length)+ans+nums[1].substring(1,equation.length));
-           }              
-          /*if(operations.includes(equation[i]))
-            {
-              console.log("Found operation.")
-              switch(equation[i])
-              {
-                case "+":
-                  setData(Number(nums[0]) + Number(nums[1]));
-                  break;
-                case "-":
-                  setData(Number(nums[0]) - Number(nums[1]));
-                  break;
-                case "*":
-                  setData(Number(nums[0]) * Number(nums[1]));
-                  break;
-                case "/":
-                  setData(Number(nums[0]) / Number(nums[1]));
-                  break;
-                case "%":
-                  setData(Number(nums[0]) % Number(nums[1]));
-                   break;
-                default:
-                  console.log("Default");         
-              }
-            }*/
-      }
+      const calculate=(equation)=>{
+           setData(eval(equation))                
     } 
     return (
         <div>
         <Display parentToChild={data}/>    
         <div class='grid-container'>
          <button class='grid-item' onClick={() => parentToChild("%")}>%</button>
-         <button class='grid-item' onClick={() => parentToChild("^2")}>x^2</button>
+         <button class='grid-item' onClick={() => parentToChild("**")}>x^n</button>
          <button class='grid-item' onClick={() => parentToChild("sqrt")}>sqrt x</button>  
          <button class='grid-item' onClick={() => parentToChild("/")}>/</button>
          <button class='grid-item' onClick={() => parentToChild("7")}>7</button>
