7c881731c8d65de59d853f4c9cd4c27f2449632d
Akshada Tambe <akshada.tambe33@gmail.com>
2024-06-03 23:17:06 +0530

    Added other Math operations

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 function Grid() {
     let [data, setData] = useState("");
-
+    let operations=["+","-","*","-","/","%"];
     const parentToChild = (a) => {
         setData(data + a);
         console.log("data "+data);
@@ -13,14 +13,36 @@ function Grid() {
       const clear = () => {
         setData("");
       }
-      const answer = (equation) => {
-        console.log("equation "+equation.toString());
-        let nums=equation.toString().split("+");
-        console.log(Number(nums[0]));
-        console.log(nums[1]);
-        setData(Number(nums[0]) + Number(nums[1]));
-        console.log("Data  "+data)
-      }  
+      const calculate=(equation)=>{
+        for (let i = 0; i < equation.length; i++) {
+          console.log(equation[i]);
+          let nums=equation.toString().split(equation[i]);
+          if(operations.includes(equation[i]))
+            {
+              console.log("Found operation.")
+              switch(equation[i])
+              {
+                case "+":
+                  setData(Number(nums[0]) + Number(nums[1]));
+                  break;
+                case "-":
+                  setData(Number(nums[0]) - Number(nums[1]));
+                  break;
+                case "*":
+                  setData(Number(nums[0]) * Number(nums[1]));
+                  break;
+                case "/":
+                  setData(Number(nums[0]) / Number(nums[1]));
+                  break;
+                case "%":
+                  setData(Number(nums[0]) % Number(nums[1]));
+                   break;
+                default:
+                  console.log("Default");         
+              }
+            }
+      }
+    } 
     return (
         <div>
         <Display parentToChild={data}/>    
@@ -44,7 +66,7 @@ function Grid() {
          <button class='grid-item'onClick={() => clear()}>clear</button>
          <button class='grid-item' onClick={() => parentToChild("0")}>0</button>
          <button class='grid-item' onClick={() => parentToChild(".")}>.</button>
-         <button class='grid-item' onClick={() => answer(data)}>=</button> 
+         <button class='grid-item' onClick={() => calculate(data)}>=</button> 
         </div>
         </div>
     );
